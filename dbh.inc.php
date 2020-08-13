d2239963ffce9fdceb967b460b5a0caf80d03c93
Sho <shoishikawasocial@gmail.com>
2020-08-14 00:45:55 +0900

    testing clear db

--- dbh.inc.php
+++ dbh.inc.php
@@ -1,9 +1,15 @@
 
 <?php
 
+/* dev
 $dsn ="mysql:dbname=latelur;host=localhost";
 $username = "root";
 $password = "";
+*/
+
+$dsn = "mysql:dbname=heroku_2f8e4059c2be9d3;host=us-cdbr-east-02.cleardb.com";
+$username = "b8875702473307";
+$password = "47bb3b33";
 
 try{
     $conn = new PDO($dsn,$username,$password);
